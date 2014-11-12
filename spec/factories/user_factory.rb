8ab1ef853c84ba0482254ab9398a0f13c0adf18b
Johnathan Weisner <joweisner@gmail.com>
2014-11-12 01:11:19 -0500

    Added admin flag to user.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -4,5 +4,8 @@ FactoryGirl.define do
     email "joweisner@gmail.com"
     password "BestPassword123"
     password_confirmation "BestPassword123"
+    factory :admin_user do
+      admin true
+    end
   end
 end
